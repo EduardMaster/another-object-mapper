7d7619bf91063cd0ea74b639f10025cb1a88cdaf
Eduard Dev <eduardkiller@hotmail.com>
2024-03-06 18:14:13 -0300

    first commit another object mapper

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    java
+    kotlin("jvm") version "1.9.22"
+    `maven-publish`
+}
+
+group = "org.eduard.another"
+version = "1.0"
+
+
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.targetCompatibility = JavaVersion.VERSION_1_8
+tasks {
+    compileJava{
+        options.encoding = "UTF-8"
+    }
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "org.eduard.another"
+            artifactId = "object_mapper"
+            version = project.version as String
+            from(components["java"])
+        }
+    }
+}
+
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+    mavenGoogle();
+}
+
+dependencies {
+    compileOnly(kotlin("stdlib"))
+    implementation("com.google.code.gson:gson:2.9.1")
+}
